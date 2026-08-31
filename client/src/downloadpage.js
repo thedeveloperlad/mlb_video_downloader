d5df757ff32f98ebb85318b3f5e38de39fc26b51
leonardo <lopez.leonardo@uabc.edu.mx>
2026-08-31 15:36:08 -0700

    [MLB Video Downloader Web]: Refactor of App.js implementing Navigate to switch pages when the file is ready to download or back to home.

---
+++ client/src/downloadpage.js
@@ -0,0 +1,11 @@
+function DownloadPage() {
+  return (
+    <div>
+      <h1>Download PAGE</h1>
+      <p>BRING FILES OR LINKS TO DOWNLOAD</p>
+      <button>Download VIDEO</button>
+    </div>
+  );
+}
+
+export default DownloadPage;
