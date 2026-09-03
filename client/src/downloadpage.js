d1c802f8ca7a42dd71ae9bb2d3aebd5b8ee5e418
leonardo <lopez.leonardo@uabc.edu.mx>
2026-09-03 08:52:11 -0700

    [MLB Video Downloader Web]: Added Download button action to download MLB file on download folder.

--- client/src/downloadpage.js
+++ client/src/downloadpage.js
@@ -1,4 +1,5 @@
 import { useLocation  } from 'react-router-dom';
+import React, { useState } from 'react';
 
 function DownloadPage() {
 
@@ -6,9 +7,43 @@ function DownloadPage() {
 
   const dataRecieve = mlbData.state;
 
+  const [isDownloading, setIsDownloading] = useState(false);
+
   if (!dataRecieve) {
     return <p>Not data.</p>;
   }
+
+  const handleDownload = async (fileUrl, name) => {
+    setIsDownloading(true); // Start loading state
+    console.log("File URL: " + fileUrl);
+    console.log("File Name: " + name);
+
+    const videoUrl = fileUrl; 
+    const fileName = name + '.mp4';
+
+    try {
+      const response = await fetch(videoUrl);
+      const blob = await response.blob(); 
+      
+      const url = window.URL.createObjectURL(blob); // Creates a temporary local hidden address for the data
+      const link = document.createElement('a');
+      
+      link.href = url;
+      link.setAttribute('download', fileName);
+      document.body.appendChild(link);
+      
+      link.click(); // Triggers the direct download
+      
+      // Clean up the DOM and memory
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error('Video download failed:', error);
+      alert('Could not download video. See console for details.');
+    } finally {
+      setIsDownloading(false); // Stop loading state (runs even if it fails)
+    }
+  };
 
   return (
     <div class="container">
@@ -18,9 +53,9 @@ function DownloadPage() {
         <div class="item item-grow-1">Name: {dataRecieve.name}</div>
         {/* <div class="item item-grow-1">Source: {dataRecieve.source}</div> */}
         <div class="item item-grow-1">
-          <a href={dataRecieve.source} download={dataRecieve.name+".mp4"}>
-            <button class="btn btn-lg btn-primary"> Download Video File </button>
-          </a>
+            <button class="btn btn-lg btn-primary" onClick={() => handleDownload(dataRecieve.source, dataRecieve.name)}  disabled={isDownloading}>
+               {isDownloading ? 'Downloading video...' : 'Download MP4 Video File'}
+               </button>
         </div>
 
         {/*<div>Source: {jsonRes.data.originalData.source}</div>*/}
